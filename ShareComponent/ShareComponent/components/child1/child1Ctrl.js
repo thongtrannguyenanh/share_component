e669ae4080d264676d0857f06c8e0c206260178b
Thong Tran Nguyen Anh <thong.tran@orientsoftware.net>
2016-09-13 16:00:38 +0700

    first commit

---
+++ ShareComponent/ShareComponent/components/child1/child1Ctrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('shareComponent')
+        .controller('child1Ctrl', child1Ctrl);
+
+    function child1Ctrl() {
+        var vm = this;
+
+        this.$onInit = function () {
+            vm.data = this.parent.child1Data;
+            vm.ExternalClick = this.parent.child1ExternalClick;
+        };
+
+    };
+})();
